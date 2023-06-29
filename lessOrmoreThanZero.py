4be63989affd491faee9683f9bd73bbb8a27ddad
HananAlradadi <87634898+HananAlradadi@users.noreply.github.com>
2023-06-29 07:29:25 +0300

    Create lessOrmoreThanZero.py

---
+++ lessOrmoreThanZero.py
@@ -0,0 +1,8 @@
+from typing import List
+def less_or_more_than_zero(number: int) -> str:
+    # write your code here ^_^
+    if number > 0 :
+        return 'Greater than zero'
+    if number < 0 :
+        return 'Less than zero'
+    return 'Equal to zero'
