bfe9c8e248d6bcf9ea5ff768ffbf1513d2c4817e
HananAlradadi <87634898+HananAlradadi@users.noreply.github.com>
2023-06-29 07:38:59 +0300

    Create returnStringLetters.py

---
+++ returnStringLetters.py
@@ -0,0 +1,6 @@
+from typing import List
+def returnStringLetters(string1: str, string2: str) -> int:
+    # write your code here ^_^
+    if len(string1) > len(string2):
+        return len(string1)
+    return len(string2)
