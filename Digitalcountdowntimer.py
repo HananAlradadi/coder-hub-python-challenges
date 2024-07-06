008448fe0d39c93b8f43c1c277086e1610ae92f2
HananAlradadi <87634898+HananAlradadi@users.noreply.github.com>
2024-07-06 17:31:31 +0300

    Update Digitalcountdowntimer.py

--- Digitalcountdowntimer.py
+++ Digitalcountdowntimer.py
@@ -2,7 +2,7 @@ from typing import List
 def countDown(number: int) -> str:
     # write your code here ^_^
     if number >= 0 : 
-        countdown_list = list(range(number, number *-1, number *-1))
+        countdown_list = list(range(number, -1, -1))
     else :
         countdown_list = list(range(number, 1, 1))
     return ' '.join(map(str, countdown_list))
